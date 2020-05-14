a9b41ffabcaa680f96c8377d929948ced878d6c5
Danielle Spencer <d.m.spencer.0486@gmail.com>
2020-05-14 13:26:43 -0500

    user registration

--- config/db.js
+++ config/db.js
@@ -6,9 +6,10 @@ const connectDB = async () => {
     try {
         await mongoose.connect(db, {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
+            useCreateIndex: true
         });
-        
+
         console.log('MongoDB Connected ...')
     } catch (err) {
         console.error(err.message);
