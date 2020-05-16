ace51f41c1d00f8aa5e890f15ac287d28a90685d
Danielle Spencer <d.m.spencer.0486@gmail.com>
2020-05-16 13:58:35 -0500

    load user and set auth token

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,24 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
+
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR } from './types';
+
+// Load user
+export const loadUser = () => async dispatch => {
+
+    try {
+        const res = await axios.get('/api/auth');
+
+        dispatch({
+            type: USER_LOADED,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: AUTH_ERROR
+        })
+    }
+}
 
 // Register user
 export const register = ({ name, email, password}) => async dispatch => {
