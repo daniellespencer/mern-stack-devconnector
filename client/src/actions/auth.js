61a00a005b8193e97043f3ce91284f9ab44c9714
Danielle Spencer <d.m.spencer.0486@gmail.com>
2020-05-18 10:57:29 -0500

    started dashboard component

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthToken';
 
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT } from './types';
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT, CLEAR_PROFILE } from './types';
 
 // Load user
 export const loadUser = () => async dispatch => {
@@ -88,5 +88,6 @@ export const login = ( email, password) => async dispatch => {
 
 // Logout / Clear profile
 export const logout = () => dispatch => {
+    dispatch({ type: CLEAR_PROFILE });
     dispatch({ type: LOGOUT });
 }
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,14 +4,18 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types'
 import { logout } from '../../actions/auth';
 
+
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
-        <li><a onClick={logout} href="#!">
-          <i className="fa fa-sign-out"></i> {' '}
-          <span className='hide-sm'>Logout</span></a>
-        </li>
-      </ul>
+      <li><Link to="/dashboard">
+      <i className="fa fa-user"></i> {' '}
+      <span className='hide-sm'> Dashboard</span></Link></li>
+      <li><a onClick={logout} href="#!">
+        <i className="fa fa-sign-out"></i> {' '}
+        <span className='hide-sm'>Logout</span></a>
+      </li>
+    </ul>
   );
 
   const guestLinks = (
