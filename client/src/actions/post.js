9681f096e030fdfd0aaee82e6bc5cf7b551593d8
Danielle Spencer <d.m.spencer.0486@gmail.com>
2020-05-22 18:17:32 -0500

    like and unlike functionality

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { setAlert } from './alert';
 import {
     GET_POSTS,
-    POST_ERROR
+    POST_ERROR,
+    UPDATE_LIKES
 } from './types';
 
 // Get Posts
@@ -20,4 +21,38 @@ export const getPosts = () => async dispatch => {
             payload: { msg: err.response.statusText, status: err.response.status }
         })
     }
-}
+};
+
+// Add Like
+export const addLike = id => async dispatch => {
+    try {
+        const res = await axios.put(`/api/posts/like/${id}`);
+
+        dispatch({
+            type: UPDATE_LIKES,
+            payload: { id, likes: res.data}
+        });
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+};
+
+// Remove Like
+export const removeLike = id => async dispatch => {
+    try {
+        const res = await axios.put(`/api/posts/unlike/${id}`);
+
+        dispatch({
+            type: UPDATE_LIKES,
+            payload: { id, likes: res.data}
+        });
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+};
